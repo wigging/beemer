7206f23d6489d1304f670eec7672d6dcefc3a350
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-04-08 21:26:50 -0400

    Add example for CaClient and EPICS container

---
+++ examples/basic_epics.py
@@ -0,0 +1,25 @@
+"""
+Example of using the `CaClient` class to get PV values from the EPICS Docker
+container. Follow the steps below to run the container and this example.
+
+1) Start up Docker
+2) Follow steps in the Dockerfile to run the EPICS container
+3) Run this example with `python basic_epics.py`
+"""
+
+from beemer import CaClient
+
+
+def main():
+    """
+    Run this example.
+    """
+    client = CaClient()
+    values = client.get_pv_values("temperature:water")
+
+    for key, value in values.items():
+        print(f"PV {key} value is {value}")
+
+
+if __name__ == "__main__":
+    main()
